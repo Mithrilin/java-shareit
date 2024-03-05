6b490943c9d5afa0cfa0b5e7c8e30298b03c59ea
Roman <romanukyan86@yandex.ru>
2024-03-06 00:21:22 +0300

    добавить изменения

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,11 +2,16 @@ package ru.practicum.shareit.user.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     private String name;
--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,20 +1,21 @@
 package ru.practicum.shareit.user.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    User addUser(User user);
-
-    boolean isEmailPresent(String userEmail);
-
-    User getUserById(Long id);
-
-    void changeEmailInMap(String newEmail, String oldEmail);
-
-    void deleteUser(long id);
-
-    List<User> getAllUsers();
+//    User addUser(User user);
+//
+//    boolean isEmailPresent(String userEmail);
+//
+//    User getUserById(Long id);
+//
+//    void changeEmailInMap(String newEmail, String oldEmail);
+//
+//    void deleteUser(long id);
+//
+//    List<User> getAllUsers();
 }
